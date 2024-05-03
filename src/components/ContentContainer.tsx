acfeb956da88aeae13712bac540443bf7d04b5c8
Lauren-Nicole <100797990+CluelessBiker@users.noreply.github.com>
2024-05-03 12:51:12 +0300

    feat(ContentContainer.tsx): create element & styling

---
+++ src/components/ContentContainer.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const ContentContainer = styled.div`
+  display: flex;
+  max-width: 500px;
+  gap: var(--spacing-1);
+  flex-direction: column;
+  padding: var(--spacing-2);
+  border-radius: var(--spacing-2);
+  background-color: var(--palette-primary-white);
+`;
