179c3c3d68f8027090b628a4abf991e370871427
Lauren-Nicole <100797990+CluelessBiker@users.noreply.github.com>
2024-05-03 12:47:32 +0300

    fix(App.css): set up page routing & check user logged in status

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import './App.css';
+import { Routes, Route, useNavigate } from 'react-router-dom';
+import LoginPage from './pages/LoginPage';
+import { ContentContainer } from './components/ContentContainer';
+import { useCurrentUser } from './context/CurrentUserContext';
 
 function App() {
-  return <div></div>;
+  const currentUser = useCurrentUser();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!currentUser) navigate('/login');
+  });
+
+  return (
+    <div className={'bodyContainer'}>
+      <ContentContainer>
+        <Routes>
+          <Route path={'/'} element={<h1>home</h1>} />
+          <Route path={'/login'} element={<LoginPage />} />
+        </Routes>
+      </ContentContainer>
+    </div>
+  );
 }
 
 export default App;
