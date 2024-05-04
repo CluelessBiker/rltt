4dd3ba2969e449908a1603c9623a7578d7facd4e
Lauren-Nicole <100797990+CluelessBiker@users.noreply.github.com>
2024-05-04 10:47:11 +0300

    fix(Button.tsx): update style rules & naming convention

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,33 +2,40 @@ import React, { FC } from 'react';
 import styled from 'styled-components';
 
 interface ButtonProps {
+  padding: string;
   isDisabled: boolean;
 }
 
-const Buttons = styled.button<ButtonProps>`
+const StyledButton = styled.button<ButtonProps>`
   border: none;
   text-transform: uppercase;
-  padding: var(--spacing-2);
   border-radius: var(--spacing-2);
   color: var(--palette-primary-cotton);
   font-size: var(--typography-size-caption);
   font-weight: var(--typography-weight-bold);
   font-family: var(--typography-face-primary);
   background-color: var(--palette-primary-darkGrey);
+  padding: ${(props) => props.padding};
   opacity: ${(props) => (props.isDisabled ? 0.5 : 1)};
 `;
 
 type Props = {
   text: string;
+  padding?: string;
   disabled?: boolean;
   onClick: () => void;
 };
 
-const Button: FC<Props> = ({ text, disabled = false, onClick }) => {
+const Button: FC<Props> = ({ text, padding, disabled = false, onClick }) => {
   return (
-    <Buttons onClick={onClick} disabled={disabled} isDisabled={disabled}>
+    <StyledButton
+      onClick={onClick}
+      disabled={disabled}
+      isDisabled={disabled}
+      padding={padding ? padding : 'var(--spacing-2)'}
+    >
       {text}
-    </Buttons>
+    </StyledButton>
   );
 };
 
