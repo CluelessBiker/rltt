73758bb66bbe247cef6469fc33eb01862ee944d0
Lauren-Nicole <100797990+CluelessBiker@users.noreply.github.com>
2024-05-04 10:46:39 +0300

    fix(App.tsx):import user context & TodoListPage component

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,16 @@ import './App.css';
 import { Routes, Route, useNavigate } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 import { ContentContainer } from './components/ContentContainer';
-import { useCurrentUser } from './context/CurrentUserContext';
+import {
+  useCurrentUser,
+  useSetCurrentUser,
+} from './context/CurrentUserContext';
+import Button from './components/Button';
+import TodoListPage from './pages/TodoListPage';
 
 function App() {
   const currentUser = useCurrentUser();
+  const setCurrentUser = useSetCurrentUser();
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -16,8 +22,17 @@ function App() {
   return (
     <div className={'bodyContainer'}>
       <ContentContainer>
+        {currentUser && (
+          <div className={'logoutButton'}>
+            <Button
+              text={'logout'}
+              padding={'var(--spacing-1)'}
+              onClick={() => setCurrentUser?.(null)}
+            />
+          </div>
+        )}
         <Routes>
-          <Route path={'/'} element={<h1>home</h1>} />
+          <Route path={'/'} element={<TodoListPage />} />
           <Route path={'/login'} element={<LoginPage />} />
         </Routes>
       </ContentContainer>
