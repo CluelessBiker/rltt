0a62c8de85c357fce531e5bea0b3a77f13318056
Lauren-Nicole <100797990+CluelessBiker@users.noreply.github.com>
2024-05-04 10:58:09 +0300

    fix(User.ts):update typeset

--- src/types/User.ts
+++ src/types/User.ts
@@ -1,10 +1,11 @@
 export type User = {
-  id: number;
-  username: string;
-  email: string;
-  firstName: string;
-  lastName: string;
-  gender: string;
-  image: string;
-  token: string;
+  user_id: number;
+  user_email: string;
+  user_name: string;
+  user_is_active: number;
+  user_profile_image: string;
+  user_last_active_epoch: number;
+  user_creation_epoch: number;
+  user_is_new: number;
+  user_token: string;
 };
