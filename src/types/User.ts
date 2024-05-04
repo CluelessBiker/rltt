9b46c1c98b80c509f95661af95e48ba3579c48fe
Lauren-Nicole <100797990+CluelessBiker@users.noreply.github.com>
2024-05-04 10:52:51 +0300

    fix(User.ts): remove unused types

--- src/types/User.ts
+++ src/types/User.ts
@@ -1,13 +1,4 @@
 export type User = {
-  // user_id: number;
-  // user_email: string;
-  // user_name: string;
-  // user_is_active: number;
-  // user_profile_image: string;
-  // user_last_active_epoch: number;
-  // user_creation_epoch: number;
-  // user_is_new: number;
-  // user_token: string;
   id: number;
   username: string;
   email: string;
